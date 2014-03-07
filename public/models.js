2228d9ca44b4c6d8b104cdcd794b063420c6ccb8
Tom Lieber <tom@alltom.com>
2014-03-07 01:18:38 -0800

    fix localStorage

--- public/models.js
+++ public/models.js
@@ -320,8 +320,10 @@ var Pile = Backbone.Model.extend({
 		this.comparisons = new ComparisonCollection(this.collection.store.makeStore(storeID, "comparisons"));
 		this.taskForest = new TaskForest(this.tasks, this.comparisons);
 
-		this.tasks.dropboxDatastore.syncCollection(this.tasks);
-		this.comparisons.dropboxDatastore.syncCollection(this.comparisons);
+		if (this.tasks.dropboxDatastore) {
+			this.tasks.dropboxDatastore.syncCollection(this.tasks);
+			this.comparisons.dropboxDatastore.syncCollection(this.comparisons);
+		}
 
 		this.tasks.pile = this.comparisons.pile = this;
 	},
